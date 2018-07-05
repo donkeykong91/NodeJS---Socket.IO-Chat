b07e4677384450e91eff85f47d9795cf2a8d2a57
donkeykong91 <darylnator@gmail.com>
2018-07-05 02:32:58 -0700

    Starting point

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,6 @@
-
-var socket = io("http://localhost:3000");
-
-socket.on("disconnect", function() {
-	setTitle("Disconnected");
-});
-
-socket.on("connect", function() {
-	setTitle("Connected to Cyber Chat");
-});
-
-socket.on("message", function(message) {
-	printMessage(message);
-});
-
 document.forms[0].onsubmit = function () {
     var input = document.getElementById("message");
     printMessage(input.value);
-    socket.emit("chat", input.value);
     input.value = '';
 };
 
